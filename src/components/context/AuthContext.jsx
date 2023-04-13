b0c781152791dd8ba0e9f045273adb5077378464
metablog <khu10735@naver.com>
2023-04-13 12:36:09 +0900

    imageupload

---
+++ src/components/context/AuthContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useContext } from 'react';
+import { login, logout, onUserStateChange } from '../../api/firebase';
+import { useState, useEffect } from 'react';
+const AuthContext = createContext();
+
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange((user) => {
+      console.log(user);
+      setUser(user);
+    });
+  }, []);
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuthContext() {
+  return useContext(AuthContext);
+}
