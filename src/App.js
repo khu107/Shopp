08b47319a9e70c11e1acfde0d6039f1b198e0e05
metablog <khu10735@naver.com>
2023-04-14 15:46:50 +0900

    prpduct_page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { AuthContextProvider } from './components/context/AuthContext';
-
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 function App() {
   return (
-    <AuthContextProvider>
-      <Navbar />
-      <Outlet />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <Navbar />
+        <Outlet />
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
 
